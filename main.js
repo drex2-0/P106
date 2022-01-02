238061293f7c242414d4d48b8fe02d2cdcd00802
drex2-0 <84222894+drex2-0@users.noreply.github.com>
2022-01-02 13:04:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,39 @@
-function start(){
-    navigator.mediaDevices.getUserMedia({audio:true});
-    classifier=ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/nnxt6JBFH/model.json',modelready);
+function startClassification()
+{
+  navigator.mediaDevices.getUserMedia({ audio: true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/u02fOYRBx/model.json', modelReady);
 }
 
-function modelready()
-{
-    classifier.classify(gotresults);
+function modelReady(){
+  classifier.classify( gotResults);
 }
 
-function gotresults(error,results)
-{
-    if(error)
+function gotResults(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results); 
+    r=Math.floor(Math.random()*255)+1;
+    g=Math.floor(Math.random()*255)+1;
+    b=Math.floor(Math.random()*255)+1;
+    document.getElementById("result_label").innerHTML="I can hear - " + results[0].label;
+    
+    img1=document.getElementById("animal_img");
+    
+    if(results[0].label=="Barking")
     {
-        console.error(error);
-    }else{
-        console.log(results);
+        img1.src="dog.gif";
+        
     }
-}
+    else if(results[0].label=="Meowing")
+    {
+        img1.src="cat.gif";
+        
+    } 
+    
+    
+    else
+    {
+        img1.src="ear.gif";
+       
+    } }}
